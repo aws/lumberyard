8fc50f60b4ea840e7be46001533b912b655c5710
Dan Hughes <dan.hughes@thirdkindgames.com>
2018-05-30 15:23:35 +0100

    Fix blend state copy and paste error.

--- dev/Code/CryEngine/RenderDll/XRenderD3D9/DXGL/Interfaces/CCryDXGLDeviceContext.cpp
+++ dev/Code/CryEngine/RenderDll/XRenderD3D9/DXGL/Interfaces/CCryDXGLDeviceContext.cpp
@@ -1281,7 +1281,7 @@ void CCryDXGLDeviceContext::OMGetBlendState(ID3D11BlendState** ppBlendState, FLO
     BlendFactor[0] = m_auBlendFactor[0];
     BlendFactor[1] = m_auBlendFactor[1];
     BlendFactor[2] = m_auBlendFactor[2];
-    BlendFactor[2] = m_auBlendFactor[3];
+    BlendFactor[3] = m_auBlendFactor[3];
     *pSampleMask = m_uSampleMask;
 }
 

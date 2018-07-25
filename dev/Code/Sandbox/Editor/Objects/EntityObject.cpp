ba19db42c3d7becb9c4fca9d26a2bdf275f4d695
SRugina <30350487+SRugina@users.noreply.github.com>
2018-07-25 19:02:35 +0100

    temporary changes to sub-expressions
    temporarily removed duplicated sub-expressions that did not have clear solutions. This ought to be fixed in a future update.

--- dev/Code/Sandbox/Editor/Objects/EntityObject.cpp
+++ dev/Code/Sandbox/Editor/Objects/EntityObject.cpp
@@ -1697,8 +1697,8 @@ void CEntityObject::EndEditParams(IEditor* ie)
     }
     ms_pTreePanel = NULL;
 
-    //s_pPropertiesPanel duplicated, unsure why
-    if (s_pPropertiesPanel && s_pPropertiesPanel)
+    //s_pPropertiesPanel duplicated, unsure why so temporarily removed the duplicated s_pPropertiesPanel
+    if (s_pPropertiesPanel)
     {
         s_pPropertiesPanel->ClearUpdateCallback();
     }

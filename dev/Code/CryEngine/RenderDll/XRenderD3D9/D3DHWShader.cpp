52876ff5753ea1addc3ad4eeecf47e217c21984b
SRugina <30350487+SRugina@users.noreply.github.com>
2018-07-25 20:38:13 +0100

    The '|' operator is applied to bool type value, typo?
    did you mean ||?

--- dev/Code/CryEngine/RenderDll/XRenderD3D9/D3DHWShader.cpp
+++ dev/Code/CryEngine/RenderDll/XRenderD3D9/D3DHWShader.cpp
@@ -1829,7 +1829,7 @@ namespace
                     if (gcpRendD3D->FX_GetEnabledGmemPath(nullptr))
                     {
                         uint32 stencilRef = CRenderer::CV_r_VisAreaClipLightsPerPixel ? 0 : (rRP.m_RIs[0][0]->nStencRef | BIT_STENCIL_INSIDE_CLIPVOLUME);
-                        stencilRef |= (!(rRP.m_pCurObject->m_ObjFlags & FOB_DYNAMIC_OBJECT) | CRenderer::CV_r_deferredDecalsOnDynamicObjects ? BIT_STENCIL_RESERVED : 0);
+                        stencilRef |= (!(rRP.m_pCurObject->m_ObjFlags & FOB_DYNAMIC_OBJECT) | CRenderer::CV_r_deferredDecalsOnDynamicObjects ? BIT_STENCIL_RESERVED : 0); //do you mean ||, typo?
                         result[0].f[0] = azlossy_caster(stencilRef);
                         result[0].f[1] = 0.0f;
                         result[0].f[2] = 0.0f;

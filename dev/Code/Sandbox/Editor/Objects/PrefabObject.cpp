3015cbf2acb0db531c2029e675fd4587549cd29a
Dan Hughes <dan.hughes@thirdkindgames.com>
2018-07-25 11:59:24 +0100

    LY-84672 203182, V561 It's probably better to assign value to 'foo' variable than to declare it anew.
    There is no need to redefine IDataBaseLibrary* here.

--- dev/Code/Sandbox/Editor/Objects/PrefabObject.cpp
+++ dev/Code/Sandbox/Editor/Objects/PrefabObject.cpp
@@ -1470,7 +1470,7 @@ namespace
         IDataBaseLibrary* pLibrary = GetIEditor()->GetPrefabManager()->FindLibrary(libraryName);
         if (pLibrary == NULL)
         {
-            IDataBaseLibrary* pLibrary = GetIEditor()->GetPrefabManager()->LoadLibrary(libraryName);
+            pLibrary = GetIEditor()->GetPrefabManager()->LoadLibrary(libraryName);
         }
 
         IDataBaseItem* pItem = GetIEditor()->GetPrefabManager()->FindItemByName(itemName);

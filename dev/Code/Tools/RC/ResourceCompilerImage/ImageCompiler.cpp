94d7bc58bc0a4fcd43fa2fff73567765e92537d1
SRugina <30350487+SRugina@users.noreply.github.com>
2018-12-06 20:33:05 +0000

    Added some auto-default for terrain highpasses
    added the _diffT suffix for Terrain_Albedo (RC will high pass it)
    Also added the _diffTHP for Terrain_Albedo_HighPassed (already highpassed)
    This is to make working with terrain textures easier.

--- dev/Code/Tools/RC/ResourceCompilerImage/ImageCompiler.cpp
+++ dev/Code/Tools/RC/ResourceCompilerImage/ImageCompiler.cpp
@@ -449,6 +449,8 @@ static string AutoselectPreset(const ConvertContext& CC, const uint32 width, con
     const char* const defaultPow2Alpha  = "AlbedoWithGenericAlpha";
     const char* const defaultNonpow2    = "ReferenceImage";
     const char* const defaultHeight     = "Displacement";
+    const char* const defaultTerrain   = "Terrain_Albedo";
+    const char* const defaultHighpassed   = "Terrain_Albedo_HighPassed";
 
     const string fileName = CC.config->GetAsString("overwritefilename", CC.sourceFileNameOnly, CC.sourceFileNameOnly);
 
@@ -478,6 +480,14 @@ static string AutoselectPreset(const ConvertContext& CC, const uint32 width, con
     else if (SuffixUtil::HasSuffix(fileName.c_str(), '_', "displ"))
     {
         presetName = defaultHeight;
+    }
+    else if (SuffixUtil::HasSuffix(fileName.c_str(), '_', "diffT"))
+    {
+        presetName = defaultTerrain;
+    }
+    else if (SuffixUtil::HasSuffix(fileName.c_str(), '_', "diffTHP"))
+    {
+        presetName = defaultHighpassed;
     }
     else if (SuffixUtil::HasSuffix(fileName.c_str(), '_', "cm") || (StringHelpers::EndsWithIgnoreCase(CC.sourceFileNameOnly, ".hdr")))
     {

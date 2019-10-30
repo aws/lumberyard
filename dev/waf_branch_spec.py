db222c87575bc8edfe0ed7cbdd882ed7c8b692f0
lybuilder <lybuilder@ip-0A011EDA>
2019-10-30 11:38:42 -0700

    Lumberyard Release 1.21.2.0
            Binaries Uncompressed Size: 21.69GB (23292961137 bytes)

--- dev/waf_branch_spec.py
+++ dev/waf_branch_spec.py
@@ -102,7 +102,7 @@ with open(os.path.join(SCRIPT_PATH, LUMBERYARD_ENGINE_VERSION_CONFIG_FILENAME)) 
 
 LUMBERYARD_VERSION = ENGINE_JSON_DATA.get('LumberyardVersion', '0.0.0.0').encode("ascii", "ignore")
 LUMBERYARD_COPYRIGHT_YEAR = ENGINE_JSON_DATA.get('LumberyardCopyrightYear', 2017)
-LUMBERYARD_BUILD = 979871
+LUMBERYARD_BUILD = 989844
 LUMBERYARD_ENGINE_PATH = os.path.normpath(ENGINE_JSON_DATA.get('ExternalEnginePath', '.').encode("ascii", "ignore"))
 
 # validate the Lumberyard version string above

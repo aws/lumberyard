540ab414b23171e4937517d4b9f3cde332db5b52
Justin Southard <jsouth33@gmail.com>
2020-03-25 04:13:58 -0700

    Update PropertyTreeEditorComponent.cpp

--- dev/Code/Framework/AzToolsFramework/AzToolsFramework/PropertyTreeEditor/PropertyTreeEditorComponent.cpp
+++ dev/Code/Framework/AzToolsFramework/AzToolsFramework/PropertyTreeEditor/PropertyTreeEditorComponent.cpp
@@ -23,7 +23,7 @@ namespace AzToolsFramework
         {
             if (auto serializeContext = azrtti_cast<AZ::SerializeContext*>(context))
             {
-                serializeContext->Class<PropertyTreeEditorComponent>();
+                serializeContext->Class<PropertyTreeEditorComponent, AZ::Component>();
             }
 
             if (auto behaviorContext = azrtti_cast<AZ::BehaviorContext*>(context))
